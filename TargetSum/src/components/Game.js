8e43fd192deead770a0d6cb49c7f70dc490b15be
Elija Amponsa <amponseli@gmail.com>
2022-04-24 07:12:10 -0400

    added style

--- TargetSum/src/components/Game.js
+++ TargetSum/src/components/Game.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import { View, Text, StyleSheet } from 'react-native';
 
+import RandomNumber from './RandomNumber';
+
  class Game extends Component {
-     target = 10 + Math.floor(40 *Math.random());
+     static propTyppes = {
+        randomNumberCount: PropTypes.number.isRequired,
+     };
+    
+     randomNumbers = Array
+        .from({ length: this.props.randomNumberCount })
+        .map(() => 1 + Math.floor(10 * Math.random())); 
+        target = this.randomNumbers
+        .slice(0, this.props.randomNumberCount - 2)
+        .reduce((acc, curr) => acc + curr, 0);
   render() {
     return (
      <View style={styles.container}> 
-     <Text style={styles.target}>{this.target}</Text>   
+        <Text style={styles.target}>{this.target}</Text> 
+        <View style={styles.randomContainer}>
+        {this.randomNumbers.map((randomNumber, index) => 
+            <RandomNumber key={index} number={randomNumber} />
+        )} 
+         </View>
      </View>
     );
   }
@@ -21,10 +39,17 @@ const styles = StyleSheet.create({
     },
 
     target: {
-        fontSize: 40,
-        backgroundColor: '#aaa',
-        marginHorizontal: 50,
+        fontSize: 50,
+        backgroundColor: '#bbb',
+        margin: 50,
         textAlign: 'center',
+    },
+
+    randomContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
     },
 });
 
